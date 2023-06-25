bf6a2771c406d9f45976df58d36a5d4831e02f9a
Artem <git.ak.hu@gmail.com>
2023-06-26 01:37:53 +0200

    day 6

---
+++ day-6: bitwise-operators.js
@@ -0,0 +1,31 @@
+const getMaxLessThanK = (n, k) => {
+    let max = 0;
+    for (let a = 1; a <= n; a++) {
+        for (let b = a + 1; b <= n; b++) {
+            let bitwise = a & b;
+            if(bitwise < k && bitwise > max) max = bitwise;
+        }
+    }
+    
+    return max;
+}
+
+function main() {
+    const q = +(readLine());
+    
+    for (let i = 0; i < q; i++) {
+        const [n, k] = readLine().split(' ').map(Number);
+        
+        console.log(getMaxLessThanK(n, k));
+    }
+}
+
+/* ============ Task ============
+
+We define S to be a sequence of distinct sequential integers from 1 to n; in other words, S={1, 2, ..., n}. We want to know the maximum bitwise AND value of any two integers, a and b (where a < b), in sequence S that is also less than a given integer, k.
+
+Complete the function in the editor so that given n and k, it returns the maximum a&b < k.
+
+Note: The & symbol represents the bitwise AND operator.
+
+*/
---
+++ day-6: javaScript-dates.js
@@ -0,0 +1,24 @@
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    let dayName;
+    // Write your code here
+    dayName = new Date(dateString).toLocaleDateString('en-US', { weekday: 'long' });
+    return dayName;
+}
+
+
+function main() {
+    const d = +(readLine());
+    
+    for (let i = 0; i < d; i++) {
+        const date = readLine();
+        
+        console.log(getDayName(date));
+    }
+}
+
+/* ============ Task ============
+
+Given a date string, dateString, in the format MM/DD/YYYY, find and return the day name for that date. Each day name must be one of the following strings: Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, or Saturday. For example, the day name for the date 12/07/2016 is Wednesday.
+
+*/
